a117a2e952ef4a1a215f4acb516b96ccba882475
Bahram Aghaei <aghaee.bahram@gmail.com>
2018-12-28 23:12:55 +0330

    improve fastdict

--- src/multitranslator/translators/fastdict.py
+++ src/multitranslator/translators/fastdict.py
@@ -1,25 +1,28 @@
+import os
+
 from .translator import Translator
 
 
 class Fastdict(Translator):
     """Download the meaning of WORD from http://fastdic.com/word"""
-    def __init__(self, word):
-        super().__init__(self, word)
-        self.name = "FastDic"
-        self.url = "https://fastdic.com/word/"
+    def __init__(self, params={}, headers={}):
+        super().__init__(params, headers)
+        self.name = "fastdic"
+        self.base_url = "https://fastdic.com"
+
+    def initialize_url(self):
+        self.new_url = os.path.join(self.base_url, 'word', word)
 
     def parse_data(self, word):
-        response = self._get_data(word)
-        if response:
-            soup = BeautifulSoup(response, 'lxml')
-            meaning = soup.find_all('ul', {'class': ['result', 'js-result']})
-            if meaning:
-                meaning = BeautifulSoup(str(meaning), 'lxml')
-                for span in meaning.find_all('span'):
-                    span.extract()
-                meaning = meaning.select('li')[0].getText()
-                meaning = str(meaning).strip().split('\n')
-                mean = str(meaning[0]).split('،')
-                return {'Persian_Mean': mean}
-        return {}
-
+        if response is None:
+            return {}
+        soup = BeautifulSoup(response, 'lxml')
+        meaning = soup.find_all('ul', {'class': ['result', 'js-result']})
+        if meaning:
+            meaning = BeautifulSoup(str(meaning), 'lxml')
+            for span in meaning.find_all('span'):
+                span.extract()
+            meaning = meaning.select('li')[0].getText()
+            meaning = str(meaning).strip().split('\n')
+            mean = str(meaning[0]).split('،')
+            return {'Persian_Mean': mean}
